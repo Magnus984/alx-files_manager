66ee4f67d370779caae0535e876b70b0eb76e9f8
Magnus <nharnharyhaw984@gmail.com>
2024-12-12 00:05:54 +0000

    Fix

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,15 @@
 import { MongoClient } from 'mongodb';
-import { config } from 'dotenv';
-
-config();
 
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'alx-files_manager';
+    const database = process.env.DB_DATABASE || 'files_manager';
     const URI = `mongodb://${host}:${port}/${database}`;
 
     this.client = new MongoClient(URI, { useUnifiedTopology: true });
     this.client.connect();
+    this.db = this.client.db(database);
   }
 
   isAlive() {
@@ -19,11 +17,11 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.client.db().collection('users').countDocuments();
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.client.db().collection('files').countDocuments();
+    return this.db.collection('files').countDocuments();
   }
 }
 
