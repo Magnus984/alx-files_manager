452f1221396c0c6fef50d2a3eda0539eae67c6c9
Magnus <nharnharyhaw984@gmail.com>
2024-12-12 01:14:47 +0000

    Fix: Changed port number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import indexRoutes from './routes/index';
 
 const app = express();
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 5000;
 
 app.use('/', indexRoutes);
 
