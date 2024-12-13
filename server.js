5c85e631d0ee537c1bc2c41c48eea934b3fbef59
Magnus <nharnharyhaw984@gmail.com>
2024-12-13 20:51:39 +0000

    Mod: Increased request payload limit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import indexRoutes from './routes/index';
 const app = express();
 const PORT = process.env.SERVER_PORT || 5000;
 
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use('/', indexRoutes);
 
 app.listen(PORT, () => {
